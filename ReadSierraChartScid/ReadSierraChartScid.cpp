15314d43bf2da3a9d5177e91d04dfd8a87f6798e
lel48 <lel48@DESKTOP-81ERLT6>
2021-08-01 11:27:42 -0400

    Add project files.

---
+++ ReadSierraChartScid/ReadSierraChartScid.cpp
@@ -0,0 +1,12 @@
+﻿// ReadSierraChartScid.cpp : Defines the entry point for the application.
+//
+
+#include "ReadSierraChartScid.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
