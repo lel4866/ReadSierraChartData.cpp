eb349f121d1a145191d6417c1ce5ba40b127145c
Lawrence Lewis <lel486@gmail.com>
2021-08-08 13:01:08 -0400

    parallelize using std::threads

--- ReadSierraChartScid/ReadSierraChartScid.cpp
+++ ReadSierraChartScid/ReadSierraChartScid.cpp
@@ -8,9 +8,12 @@
 #include <ctime>
 #include <filesystem>
 #include <fstream>
+#include <thread>
 #include "ReadSierraChartScid.h"
 
 using namespace std;
+
+void processScidFile(const string futures_root, const filesystem::path path, const string& datafile_outdir);
 std::tm* getLocalTimeFromtSCDateTime(const SCDateTime& utcTime);
 static SCDateTime getUTCTimeFromLocalTime(int year, int month, int day, int hour, int min, int sec);
 
@@ -34,6 +37,7 @@ int main()
     const chrono::time_zone* curzone = chrono::current_zone();
 
     // get .scid files in datafile_dir
+    vector<thread> threads;
     for (const filesystem::directory_entry& dir_entry : filesystem::directory_iterator(datafile_dir)) {
         if (dir_entry.is_directory())
             continue;
@@ -41,127 +45,125 @@ int main()
         if (path.extension() != ".scid")
             continue;
 
-        // get start month, end month, start year, end year from futures file name
         const string stem(path.stem().string());
         if (!stem.starts_with(futures_root))
             continue;
         if (stem.length() < 5)
             continue;
-        const char futures_code = toupper(stem[2]);
-        if (!IsValidFuturesMonthCode(futures_code))
-            continue;
-        int end_month = GetMonthFromFuturesCode(futures_code);
-        int start_month = end_month - 3;
-        int start_year, end_year;
-        const string futures_two_digit_year = stem.substr(3, 2);
-        start_year = end_year = 2000 + atoi(futures_two_digit_year.c_str());
-        switch (futures_code) {
-        case 'H':
-            start_month = 12;
-            start_year = end_year - 1;
-            break;
-        case 'Z':
-            end_month = 3;
-            end_year++;
-            break;
-        }
 
-        // open file and position to end of file (ios::ate) so we can compute size
-        ifstream datafile;
-        datafile.open(path, ios::in | ios::binary | ios::ate);
-        if (!datafile.is_open()) {
-            cout << "Unable to open data file: " << path << endl;
-            return -1;
-        }
-
-        // get file size
-        size_t size = datafile.tellg();
-        datafile.seekg(0, ios::beg); // reset pointer to beginning of file
-
-        // read SierraChart .scid file header
-        s_IntradayFileHeader header;
-        datafile.read((char*)&header, sizeof(header));
-        if (datafile.bad()) {
-            cout << "Unable to to read data file header: " << path.filename() << endl;
-            return -1;
-        }
-        size -= sizeof(header); // set size to just that used for data
-
-        if (size % sizeof(s_IntradayRecord) != 0) {
-            cout << "Data set: " << path.filename() << " does not have even number of data records." << endl;
-            return -1;
-        }
-        const size_t num_recs = size / sizeof(s_IntradayRecord);
-
-        // create CSV file. Name is {futures_root }{futures_code}{2 digit year}.csv
-        const string out_path = datafile_outdir + futures_root + futures_code + futures_two_digit_year + ".csv";
-        ofstream csv_ostream(out_path);
-
-        // output header
-        csv_ostream << "Date,Time,Price" << endl;
-
-        // only keep ticks between start_date and end_date
-        SCDateTime start_dt = getUTCTimeFromLocalTime(start_year, start_month, 9, 18, 0, 0);
-        SCDateTime end_dt = getUTCTimeFromLocalTime(end_year, end_month, 9, 18, 0, 0);
-
-        int prev_date{ -1 };
-        int prev_time{ -1 };
-        s_IntradayRecord record;
-        for (int i = 0; i < num_recs; i++) {
-            datafile.read((char*)&record, sizeof(s_IntradayRecord));
-            if (datafile.bad()) {
-                cout << "Unable to to read data file record: " << i << endl;
-                return -1;
-            }
-
-            // throw away data earlier than start date/time
-            if (record.DateTime < start_dt)
-                continue;
-
-            // terminate loop on first data later than end date/time (assumes data is sorted)
-            if (record.DateTime >= end_dt)
-                break;
-
-
-            // throw away Saturdays
-            std::tm* localTime = getLocalTimeFromtSCDateTime(record.DateTime); // get local time of current record
-            if(localTime->tm_wday == 6)
-                continue;
-
-            // throw away data between 4:30pm and 6pm ET
-            if ((localTime->tm_hour == 16 && localTime->tm_min >= 30) or localTime->tm_hour == 17)
-                continue;
-
-            const int iDate = record.DateTime.GetDate();
-            const int iTime = record.DateTime.GetTimeInSeconds();
-
-            // only keep one tick per second
-            // if this is first tick of new second, write it to output file
-            if (iDate == prev_date and iTime == prev_time)
-                continue;
-
-            // write tick to CSV file
-            csv_ostream << format("{0:02}/{1:02}/{2},{3:02}:{4:02}:{5:02},{6:.2f}\n",
-                localTime->tm_mon+1, localTime->tm_mday+1, localTime->tm_year+1900, localTime->tm_hour, localTime->tm_min, localTime->tm_sec, record.Close);
-
-            prev_date = iDate;
-            prev_time = iTime;
-        }
-        csv_ostream.close();
-
-#if 0
-        size_t n{ 0 };
-        for (; n < num_recs; n++) {
-            datafile.read((char*)&records[n], sizeof(s_IntradayRecord));
-            if (datafile.bad()) {
-                cout << "Unable to to read data file header: " << datafile_fullfn << endl;
-                return -1;
-            }
-        }
-#endif
+        cout << "processing: " << path.filename() << endl;
+        threads.emplace_back(processScidFile, futures_root, path, datafile_outdir);
     }
 
+    for (auto&& thread : threads)
+        thread.join();
+
     return 0;
+}
+
+void processScidFile(const string futures_root, const filesystem::path path, const string& datafile_outdir) {
+    const string stem(path.stem().string());
+    const char futures_code = toupper(stem[2]);
+    if (!IsValidFuturesMonthCode(futures_code))
+        return;
+    int end_month = GetMonthFromFuturesCode(futures_code);
+    int start_month = end_month - 3;
+    int start_year, end_year;
+    const string futures_two_digit_year = stem.substr(3, 2);
+    start_year = end_year = 2000 + atoi(futures_two_digit_year.c_str());
+    switch (futures_code) {
+    case 'H':
+        start_month = 12;
+        start_year = end_year - 1;
+        break;
+    case 'Z':
+        end_month = 3;
+        end_year++;
+        break;
+    }
+
+    // open file and position to end of file (ios::ate) so we can compute size
+    ifstream datafile;
+    datafile.open(path, ios::in | ios::binary | ios::ate);
+    if (!datafile.is_open()) {
+        cout << "Unable to open data file: " << path << endl;
+        return;
+    }
+
+    // get file size
+    size_t size = datafile.tellg();
+    datafile.seekg(0, ios::beg); // reset pointer to beginning of file
+
+        // read SierraChart .scid file header
+    s_IntradayFileHeader header;
+    datafile.read((char*)&header, sizeof(header));
+    if (datafile.bad()) {
+        cout << "Unable to to read data file header: " << path.filename() << endl;
+        return;
+    }
+    size -= sizeof(header); // set size to just that used for data
+
+    if (size % sizeof(s_IntradayRecord) != 0) {
+        cout << "Data set: " << path.filename() << " does not have even number of data records." << endl;
+        return;
+    }
+    const size_t num_recs = size / sizeof(s_IntradayRecord);
+
+    // create CSV file. Name is {futures_root }{futures_code}{2 digit year}.csv
+    const string out_path = datafile_outdir + futures_root + futures_code + futures_two_digit_year + ".csv";
+    ofstream csv_ostream(out_path);
+
+    // output header
+    csv_ostream << "Date,Time,Price" << endl;
+
+    // only keep ticks between start_date and end_date
+    SCDateTime start_dt = getUTCTimeFromLocalTime(start_year, start_month, 9, 18, 0, 0);
+    SCDateTime end_dt = getUTCTimeFromLocalTime(end_year, end_month, 9, 18, 0, 0);
+
+    int prev_date{ -1 };
+    int prev_time{ -1 };
+    s_IntradayRecord record;
+    for (int i = 0; i < num_recs; i++) {
+        datafile.read((char*)&record, sizeof(s_IntradayRecord));
+        if (datafile.bad()) {
+            cout << "Unable to to read data file record: " << i << endl;
+            return;
+        }
+
+        // throw away data earlier than start date/time
+        if (record.DateTime < start_dt)
+            continue;
+
+        // terminate loop on first data later than end date/time (assumes data is sorted)
+        if (record.DateTime >= end_dt)
+            break;
+
+
+        // throw away Saturdays
+        std::tm* localTime = getLocalTimeFromtSCDateTime(record.DateTime); // get local time of current record
+        if (localTime->tm_wday == 6)
+            continue;
+
+        // throw away data between 4:30pm and 6pm ET
+        if ((localTime->tm_hour == 16 && localTime->tm_min >= 30) or localTime->tm_hour == 17)
+            continue;
+
+        const int iDate = record.DateTime.GetDate();
+        const int iTime = record.DateTime.GetTimeInSeconds();
+
+        // only keep one tick per second
+        // if this is first tick of new second, write it to output file
+        if (iDate == prev_date and iTime == prev_time)
+            continue;
+
+        // write tick to CSV file
+        csv_ostream << format("{0:02}/{1:02}/{2},{3:02}:{4:02}:{5:02},{6:.2f}\n",
+            localTime->tm_mon + 1, localTime->tm_mday + 1, localTime->tm_year + 1900, localTime->tm_hour, localTime->tm_min, localTime->tm_sec, record.Close);
+
+        prev_date = iDate;
+        prev_time = iTime;
+    }
+    csv_ostream.close();
 }
 
 SCDateTime getUTCTimeFromLocalTime(int year, int month, int day, int hour, int min, int sec) {
